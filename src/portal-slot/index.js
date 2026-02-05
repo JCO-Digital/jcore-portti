c1abedf0e65a67bd5c4ff9e41e7929d5c4acf663
Niklas Schönberg <niklas@uplink.fi>
2026-02-05 18:05:13 +0200

    fix(portal-slot): enable saving of inner blocks in portal-slot block

--- src/portal-slot/index.js
+++ src/portal-slot/index.js
@@ -3,23 +3,24 @@
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
  */
-import { registerBlockType } from '@wordpress/blocks';
+import { registerBlockType } from "@wordpress/blocks";
+import { InnerBlocks } from "@wordpress/block-editor";
 
 /**
  * Internal dependencies
  */
-import Edit from './edit';
-import metadata from './block.json';
+import Edit from "./edit";
+import metadata from "./block.json";
 
 /**
  * Every block starts by registering a new block type definition.
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
  */
-registerBlockType( metadata.name, {
+registerBlockType(metadata.name, {
 	/**
 	 * @see ./edit.js
 	 */
 	edit: Edit,
-	save: () => null,
-} );
+	save: () => <InnerBlocks.Content />,
+});
